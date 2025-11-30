08ed4637020f3b40f50d03cad85a8ddc93a188e3
Štěpán Rožník <stepan@roznik.cz>
2025-11-30 16:26:41 +0100

    create custom.d.ts

---
+++ custom.d.ts
@@ -0,0 +1,9 @@
+declare module 'cyrillic-to-translit-js' {
+declare module 'latinize';
+)declare module 'latinize';
+declare module 'latinize';
+  export default class CyrillicToTranslit {
+    constructor();
+    transform(input: string): string;
+  }
+}
